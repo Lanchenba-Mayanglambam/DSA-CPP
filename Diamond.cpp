f59c116e3ea7a60909e4278dc449ad461ce2cd6d
Lanchenba Mayanglambam <lanchenbamayanglambam@Lanchenbas-MacBook-Air.local>
2025-11-29 15:11:39 +0530

    adding files

---
+++ Diamond.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int x=n/2+1;
+    for(int i=1; i<=n/2+1; i++){
+        for(int k=1; k<=x-i; k++){
+            cout<<" ";
+        }
+        for(int j=1; j<=2*i-1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1; i<=n/2; i++){
+        for(int a=1; a<=i; a++){
+            cout<<" ";
+        }
+        for(int b=1; b<=n-(2*i); b++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
